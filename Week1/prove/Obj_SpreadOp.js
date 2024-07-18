b4d9fb8c8efa9116c1acae896e0060f06b69d3c7
Daniele Minnelli <daniele.minnelli@spea.com>
2024-07-18 09:02:38 +0200

    prove sul concetto di Closures

--- Week1/prove/Obj_SpreadOp.js
+++ Week1/prove/Obj_SpreadOp.js
@@ -19,3 +19,20 @@ console.log(automobile[modello]);
 // Spread Operator
 let automobile2 = {...automobile,colore: "Rosso"}
 console.log(automobile2);
+
+// Concetto di Closures - la variabile della funz esterna sopravvive in quella interna
+// simulo oggetti usando  Concetto di Closures
+function counter() {
+    let value = 0;
+    const nextVal = ()=>{
+        value++;
+        return value;
+    }
+    return nextVal;
+}
+
+const count1 = counter() ; //associo a count1 il valore restituito da nextVall, qui counter viene eseguito e imposta value = 1, nextVALUE NON VIENE ESEGUITA
+console.log(count1()) ; // chiamando count1() eseguo la funz nextVal che aggiorna e restituisce la variabile value
+console.log(count1()) ;
+console.log(count1()) ;
+
