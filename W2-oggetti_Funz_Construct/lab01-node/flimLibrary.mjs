6ec77e4d9d88f2410084f4a027a1bbfcbdeb4fe2
Daniele Minnelli <daniele.minnelli@spea.com>
2024-07-30 15:52:46 +0200

    finito es2

--- W2-oggetti_Funz_Construct/lab01-node/flimLibrary.mjs
+++ W2-oggetti_Funz_Construct/lab01-node/flimLibrary.mjs
@@ -30,6 +30,51 @@ function FilmLibrary() {
         else    
             throw new Error(`Duplicated id`);
     }
+
+    // es2.1 returns a new array containing the Films within the FilmLibrary instance sorted in
+    this.sortByDate = ()=>{
+        // nuovo array, contiene tutti i film di listFilms
+        const newArray = [...this.listFilms];
+        
+        // riordino il nuovo arrray per data
+        newArray.sort((d1,d2)=>{
+            // gestisco le date nulle
+            if(!(d1.date)) return 1;    //film con data null viene messo in fondo alla lista
+            if(!(d2.date)) return -1;
+            return d1.date.diff(d2.date, 'day')
+        });
+        return newArray;
+    }
+
+    // es2.2 deletes a Film from the FilmLibrary based on an Id received by parameter.
+    this.deleteFilm = (id)=>{
+        // creo nuovo array, uso filter per inserire nel nuovo array i film con id diverso da quello target da eliminare
+        const newList = this.listFilms.filter(function(film, index, arr) {
+            return film.id !== id;
+        })
+        // aggiorno la lista originale con il nuovo array filtrato
+        this.listFilms = newList;
+    }
+
+    // es2.3 deletes the Watch date of all the Films in the FilmLibrary.
+    this.resetDate = ()=>{
+        // mi basta usare un forEach per eliminare le date nella listFilms, non creo nuovi array
+        this.listFilms.forEach((film)=> delete film.date);
+    }
+
+    //es2.4 ordina i film per rating decrescente, esclude i film senza punteggio
+    this.getRated = ()=>{
+        // prendo la lista dei film e tengo solo quelli con rating != 0
+        const newList = this.listFilms.filter(function(film,index,arr){
+            return film.rating > 0;
+        })
+        // ordino per rating decrescente
+        newList.sort((a,b)=>{
+            return b.rating - a.rating;
+        })
+
+        return newList;
+    }
 }
 
 
@@ -39,7 +84,7 @@ function main() {
     const grams21 = new Film(2, "21 Grams", true, "2024-03-17", 4);
     const starWars = new Film(3, "Star Wars", false);
     const matrix = new Film(4, "Matrix");
-    const shrek = new Film(5, "Shrek", false, "2024-03-21", 3);
+    const shrek = new Film(5, "Shrek", false, "2024-03-21", 9);
   
     // Adding the films to the FilmLibrary
     const library = new FilmLibrary();
@@ -53,6 +98,23 @@ function main() {
     console.log("***** List of films *****");
     library.listFilms.forEach((item) => console.log(item.toString()));
   
+    // stampa film riordinati
+    console.log("\n***** List of films (sorted) *****");
+    const sortedFilms = library.sortByDate();
+    sortedFilms.forEach((el)=>console.log(el.toString()));
+
+    // elimina un film
+    //library.deleteFilm(1);
+    
+    // resetto le date
+    //library.resetDate();
+    //console.log("***** List of films modified *****");
+    //library.listFilms.forEach((item) => console.log(item.toString()));
+    console.log("\n***** List of films (rating sorted) *****");
+
+   const sortedRating =  library.getRated();
+   sortedRating.forEach((el)=>console.log(el.toString()));
+
     // Additional instruction to enable debug 
     debugger;
   }
