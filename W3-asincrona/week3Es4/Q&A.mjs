89da55208c46a4347785e2e6c8340848a3751be7
Daniele Minnelli <daniele.minnelli@spea.com>
2024-07-30 16:25:37 +0200

    correzione es

--- W3-asincrona/week3Es4/Q&A.mjs
+++ W3-asincrona/week3Es4/Q&A.mjs
@@ -1,86 +1,130 @@
 import dayjs from 'dayjs';
 import sqlite from 'sqlite3';
 
-const db = new sqlite.Database('questions.sqlite', 
-  (err)=>{if(err) throw err})
+// Apertura database e creazione oggetto
+const db = new sqlite.Database('questions.sqlite', (err) => {
+  if (err) throw err;
+});
 
-function Answer(id, text, email, date, score=0) {
+// Oggetto rappresentante le domande (con le sue proprietà e metodi)
+function Question(id, text, email, date) {
+  this.id = id;
+  this.text = text;
+  this.email = email;
+  this.date = dayjs(date);
+
+  // metodo per prendere tutte le risposte della Question instanziata
+  this.getAnswers = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT answer.*, user.email FROM answer JOIN user ON answer.authorId=user.id WHERE answer.questionId = ?';
+      db.all(sql, [this.id], (err, rows) => {
+        if (err)
+          reject(err)
+        else {
+          const answers = rows.map((ans) => new Answer(ans.id, ans.text, ans.email, ans.date, ans.score));
+          resolve(answers);
+        }
+      });
+    });
+  }
+
+  // metodo per aggiungere una nuova risposta di un autore esistente alla Question instanziata
+  this.addAnswer = (answer) => {
+    return new Promise((resolve, reject) => {
+      let sql = 'SELECT id from user WHERE email = ?';
+      db.get(sql, [answer.email], (err, row) => {
+        if (err) {
+          reject(err);
+        } else if (row !== undefined) {
+          sql = "INSERT INTO answer(text, authorId, date, score, questionId) VALUES (?, ?, DATE(?), ?, ?)";
+          db.run(sql, [answer.text, row.id, answer.date.toISOString(), answer.score, this.id], function (err) {
+            if (err)
+              reject(err);
+            else
+              resolve(this.lastID);
+          });
+        } else
+          resolve("Author not available, check the inserted email.");
+      });
+    });
+  }
+
+  // metodo per prendere tutte le top "num" risposte della Question instanziata
+  this.getTop = (num) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT answer.*, user.email FROM answer JOIN user ON answer.authorId=user.id WHERE answer.questionId = ? ORDER BY answer.score DESC LIMIT ?';
+      db.all(sql, [this.id, num], (err, rows) => {
+        if (err)
+          reject(err)
+        else {
+          const answers = rows.map((ans) => new Answer(ans.id, ans.text, ans.email, ans.date, ans.score));
+          resolve(answers);
+        }
+      });
+    });
+  }
+}
+
+// Oggetto rappresentante le risposte (con le sue proprietà)
+function Answer(id, text, email, date, score = 0) {
   this.id = id;
   this.text = text;
   this.email = email;
   this.score = score;
   this.date = dayjs(date);
-
-  this.toString = () => {
-    return `${this.email} replied '${this.text}' on ${this.date.format('YYYY-MM-DD')} and got a score of ${this.score}`;
-  }
-
 }
 
-function Question(id,text, email, date) {
-  this.id = id;
-  this.text = text;
-  this.email = email;
-  this.date = dayjs(date);
- // this.answers = []; //no longer needed,answers stored database table.
-}
-
-
+// Oggetto rappresentante la lista di domande (con i suoi metodi)
 function QuestionList() {
-  // returns a Promise that resolves to a `Question` with the given id
-  this.getQuestion = function(questionId) {
-    return new Promise((resolve,reject)=>{
-      const sql = `select q.id, q.text, u.email, q.date
-      from question q, "user" u
-      where q.id = ?
-      and q.authorId = u.id `
 
-      db.get(sql, [questionId], (err, row)=>{
-        if(err)
-          reject(err)
-        else 
-          resolve(new Question(row.id, row.text, row.email, dayjs(row.date)))
-      })
-    })
+  // metodo per recuperare una singola Question dato il suo ID
+  this.getQuestion = function (id) {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT question.*, user.email FROM question JOIN user ON question.authorId = user.id WHERE question.id = ?';
+      db.get(sql, [id], (err, row) => {
+        if (err) {
+          reject(err);
+        } else if (row !== undefined) {
+          resolve(new Question(row.id, row.text, row.email, row.date));
+        } else {
+          resolve("Question not available, check the inserted id.");
+        }
+      });
+    });
   }
 
-  // restituisce una promise contenente email associata a user.id
-  this.getUserId = function(email) {
-    return new Promise((resolve, reject)=>{
-      const sql = `select u.id 
-      from "user" u 
-      where u.email = ?`
-      db.get(sql, [email], (err, row)=>{
-        if(err)
-          reject(err)
-        else
-          resolve(Number(row.id))
-      })
-    })
-  }
-
-  // pass a fully-constructed `Question` object.
-  this.addQuestion = function(q) {
-    const userEmail = q.email
-    const userIdPromise = this.getUserId(userEmail)
-    return userIdPromise.then((userId)=>{
-      return new Promise((resolve,reject)=>{
-        const sql = `insert into question(id, text, authorId, date) values(?,?,?,?)`
-        db.run(sql, [q.id, q.text, userId, q.date.format('YYYY-MM-DD')],
-        (err) => {
-          if(err) reject(err)
-          else resolve()
-        })
-      })
-    })
+  // metodo per aggiungere una nuova Question di un autore esistente
+  this.addQuestion = (question) => {
+    return new Promise((resolve, reject) => {
+      let sql = 'SELECT id from user WHERE email = ?';
+      db.get(sql, [question.email], (err, row) => {
+        if (err) {
+          reject(err);
+        } else if (row !== undefined) {
+          sql = 'INSERT INTO question(text, authorId, date) VALUES(?,?,DATE(?))';
+          db.run(sql, [question.text, row.id, question.date.toISOString()], function (err) {
+            if (err)
+              reject(err);
+            else
+              resolve(this.lastID);
+          });
+        } else
+          resolve("Author not available, check the inserted email.");
+      });
+    });
   }
 }
 
+// funzione asincrona per il test
+async function main() {
+  const ql = new QuestionList();
 
-const qlist = new QuestionList()
-const q1 = qlist.getQuestion(1)
-q1.then((q)=>{console.log(q.toString())})
+  const firstQuestion = await ql.getQuestion(1);
+  console.log(await firstQuestion.getTop(2));
 
-const q2 = new Question(5, 'Does it work?', 'daniele.minnelli@polito.it', dayjs())
-qlist.addQuestion(q2).then(()=>{console.log("Question added")})
+  const newQuestionId = await ql.addQuestion(new Question(undefined, "Is 1 bigger than 10?", "luigi.derussis@polito.it", dayjs()));
+  const newQuestion = await ql.getQuestion(newQuestionId);
+  console.log(await newQuestion.getAnswers());
+}
 
+main();
