cf94179d6abfd31c0fff05230123fb192e35d6b0
Daniele Minnelli <daniele.minnelli@spea.com>
2024-07-30 11:31:40 +0200

    correzione es

--- W2-oggetti_Funz_Construct/lab01-node/flimLibrary.mjs
+++ W2-oggetti_Funz_Construct/lab01-node/flimLibrary.mjs
@@ -1,13 +1,22 @@
 import dayjs from 'dayjs'
 
 // 1. Firstly, implement a constructor function to create Film objects.
-function Film(id, title, favorites=false, date=null, rating=null, idP = 1) {
+function Film(id, title, favorites=false, date=null, rating=0, idP = 1) {
     this.id = id;
     this.title = title;
     this.favorites = favorites;
-    this.date = date;
+    // saved as dayjs object only if watchDate is truthy
+    this.date = date && dayjs(date);
     this.rating = rating;
     this.idP = idP;
+
+    // funzione per stampare la lista dei fim
+    this.toString = () => {
+        return `Id: ${this.id}, ` +
+        `Title: ${this.title}, Favorite: ${this.favorites}, ` +
+        `Watch date: ${this.date}, Score: ${this.rating}, ` +
+        `User: ${this.idP}` ;
+      }
 }
 
 // 2. Secondly, implement a constructor function to create a FilmLibrary, an object containing an array of Films.
@@ -16,21 +25,36 @@ function FilmLibrary() {
 
     // 3. implement the addNewFilm method, which adds a new Film object, passed as parameter
     this.addNewFilm = (film)=>{
-        this.listFilms.push(film);
+        if(!this.listFilms.some(f => f.id == film.id))
+            this.listFilms.push(film);
+        else    
+            throw new Error(`Duplicated id`);
     }
 }
 
-// 4. Populate the FilmLibrary 
-const filmLibrary = new FilmLibrary();
-const film1 = new Film(1,"title 1", true, "10 marzo 2024", 5);
-const film2 = new Film(2,"title 2",false, "23 Luglio 2024", 8);
-const film3 = new Film(3,"title 3", true, "2 marzo 2022", 2);
 
-filmLibrary.addNewFilm(film1);
-filmLibrary.addNewFilm(film2);
-filmLibrary.addNewFilm(film3);
-
-// 5. print in the console the entire list of Films stored in the FilmLibrary
-for (const film of filmLibrary.listFilms) {
-    console.log(film);
-}
+function main() {
+    // Creating some film entries
+    const pulpFiction = new Film(1, "Pulp Fiction", true, "2024-03-10", 5);
+    const grams21 = new Film(2, "21 Grams", true, "2024-03-17", 4);
+    const starWars = new Film(3, "Star Wars", false);
+    const matrix = new Film(4, "Matrix");
+    const shrek = new Film(5, "Shrek", false, "2024-03-21", 3);
+  
+    // Adding the films to the FilmLibrary
+    const library = new FilmLibrary();
+    library.addNewFilm(pulpFiction);
+    library.addNewFilm(grams21);
+    library.addNewFilm(starWars);
+    library.addNewFilm(matrix);
+    library.addNewFilm(shrek);
+  
+    // Print Films
+    console.log("***** List of films *****");
+    library.listFilms.forEach((item) => console.log(item.toString()));
+  
+    // Additional instruction to enable debug 
+    debugger;
+  }
+  
+  main();
